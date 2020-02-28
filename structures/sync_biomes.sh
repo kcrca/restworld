f55f56469be05d8312312a16e21983f4ece8358f
Ken Arnold <arnold@peak.im>
2020-02-28 13:20:58 -0800

    clean up

--- structures/sync_biomes.sh
+++ structures/sync_biomes.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 src=$(dirname $0)/
 rwd=$HOME/clarity/home/saves/RestWorld/generated/minecraft/structures/
-<<<<<<< HEAD
 bio=$HOME/clarity/home/saves/RestWorld_old/generated/minecraft/structures/
-=======
-bio=$HOME/clarity/home/saves/Biomes1.15.2/generated/minecraft/structures/
->>>>>>> e7ac3d5bf9726990c708a9195a0b46acd4fb0779
 args="$@"
 test ${#args} = 0
 
