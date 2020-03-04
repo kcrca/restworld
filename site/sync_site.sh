d32c98c1eb71cd91c96cba943f537717f3ed7810
Ken Arnold <arnold@peak.im>
2020-03-03 19:34:39 -0800

    license update

--- site/sync_site.sh
+++ site/sync_site.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
-./build.sh
 cd `dirname $0`
+./build.sh
+./update_version.sh 1.15.2
 dst_dir="restworld"
 rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' --exclude='favicon.p*' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
--- site/update_version.sh
+++ site/update_version.sh
@@ -4,7 +4,7 @@ set -e
 
 cd `dirname $0`
 
-ver_cur="${1:-1.15.2}"
+ver_cur="$1"
 [ x"$ver_cur" != x"" ] || (echo Must specify version ; exit 1)
 ver_old=$(sed -E -n -e 's/^<.-- version: ([^ ]*) -->/\1/p' < index.html)
 ver_old_re=$(echo "$ver_old" | sed -e 's/[.]/\\./g')
@@ -18,7 +18,7 @@ g/\\<$ver_old_re\\>/s//$ver_cur/g
 /CopyrightBegin/r ../License.html
 /\\(<p class="timestamp">\\).*/s//\1Page Last Edited: $ts_cur/
 w index.html.new
-q
+q!
 EOF
 
 # If it's been changed, update the edit date
