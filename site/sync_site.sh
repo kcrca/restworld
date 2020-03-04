004d1b1c0083d45c6dddb343abc3625338dca0b8
Ken Arnold <arnold@peak.im>
2020-03-03 20:01:12 -0800

    favicon and conditional build of gifs

--- site/sync_site.sh
+++ site/sync_site.sh
@@ -3,5 +3,6 @@ set -e
 cd `dirname $0`
 ./build.sh
 ./update_version.sh 1.15.2
+./favicon.sh
 dst_dir="restworld"
 rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' --exclude='favicon.p*' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
