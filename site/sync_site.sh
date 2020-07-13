cb8fe7e9b96b271806dea3321c6730fa58395edb
Ken Arnold <arnold@peak.im>
2020-07-12 17:06:02 -0700

    prep to release

--- site/sync_site.sh
+++ site/sync_site.sh
@@ -2,7 +2,7 @@
 set -e
 cd `dirname $0`
 ./build.sh
-./update_version.sh 1.15.2
+./update_version.sh 1.16.1
 ./favicon.sh
 dst_dir="restworld"
 rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' --exclude='favicon.p*' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
