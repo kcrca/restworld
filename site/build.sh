7b3540ee4137e1191f07ffa63ebe608116357da8
Ken Arnold <arnold@peak.im>
2020-07-13 19:16:45 -0700

    update the images for the web site.

--- site/build.sh
+++ site/build.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 for f in src/*/; do
     name=$(dirname $f/.)
     out=$(basename $name)
-    if find $name -newer $out.gif | grep -q . > /dev/null ; then
+    if test ! -f $out.gif || find $name -newer $out.gif | grep -q . > /dev/null ; then
 	case $out in
 	    download)
 		echo - convert -delay 200 $name/\*.png -loop 0 -resize 32x32 $out.gif
