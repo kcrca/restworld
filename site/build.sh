ca19be36a197f9f634f85975237c7e1caa1e19ae
Ken Arnold <arnold@peak.im>
2021-06-15 17:46:53 -0700

    sample pics for site upload

--- site/build.sh
+++ site/build.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
+std_size=750x483
 cd $(dirname $0)
 for f in src/*/; do
-    name=$(dirname $f/.)
-    out=$(basename $name)
-    if test ! -f $out.gif || find $name -newer $out.gif | grep -q . > /dev/null ; then
-	case $out in
-	    download) size=32x32;;
-	    *) size=750x483
-	esac
-	echo - convert -delay 200 $name/\*.png -loop 0 -resize $size $out.gif
-	ls $name/*.png
-	convert -delay 200 $name/*.png -loop 0 -scale $size $out.gif
+    if test "$(basename $f)" = "sample_pics"; then
+	for img in "$f"/*.png; do
+	    out="sample_pics/$(basename $img)"
+	    if test "$img" -nt "$out"; then
+		echo convert $img -scale $std_size $out
+		convert $img -scale $std_size $out
+	    fi
+	done
+    else
+	name=$(dirname $f/.)
+	out=$(basename $name)
+	if test ! -f $out.gif || find $name -newer $out.gif | grep -q . > /dev/null ; then
+	    case $out in
+		download) size=32x32;;
+		*) size=$std_size
+	    esac
+	    echo - convert -delay 200 $name/\*.png -loop 0 -resize $size $out.gif
+	    ls $name/*.png
+	    convert -delay 200 $name/*.png -loop 0 -scale $size $out.gif
+	fi
     fi
 done
