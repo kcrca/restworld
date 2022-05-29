bee8bda036634bd141dc5b47ddd468a552ce738d
Ken Arnold <arnold@peak.im>
2022-05-29 00:03:42 -0700

    generalize getting enums from pages a little bit

--- restworld/build_enums.py
+++ restworld/build_enums.py
@@ -1,11 +1,12 @@
 import re
+from abc import ABC, abstractmethod
 from contextlib import redirect_stdout
 
 import requests
 from bs4 import BeautifulSoup
 
 
-class EnumDesc:
+class EnumDesc(ABC):
     def __init__(self, name, url):
         self.name = name
         self.url = url
@@ -20,6 +21,59 @@ class EnumDesc:
     def replace(self, name: str, string: str):
         return name
 
+    @abstractmethod
+    def is_name_col(self, text: str):
+        pass
+
+    @abstractmethod
+    def is_string_col(self, text: str):
+        pass
+
+    @abstractmethod
+    def is_desc_col(self, text: str):
+        pass
+
+    def is_filter_col(self, text: str):
+        return False
+
+    def filter_out(self, text: str):
+        return True
+
+    def generate(self):
+        soup = self.fetch()
+        tables = soup.find_all('table', attrs={'data-description': 'advancements'})
+        found = {}
+        filter_col = None
+        for table in tables:
+            rows = table.find_all('tr')
+            for row in rows:
+                headers = row.find_all('th')
+                if headers:
+                    for i in range(0, len(headers)):
+                        th = headers[i]
+                        text = th.text.strip()
+                        if self.is_name_col(text):
+                            name_col = i
+                        elif self.is_string_col(text):
+                            str_col = i
+                        elif self.is_desc_col(text):
+                            desc_col = i
+                        elif self.is_filter_col(text):
+                            filter_col = i
+                else:
+                    cells = row.find_all('td')
+                    if filter_col is not None and self.filter_out(cells[filter_col]):
+                        continue
+                    raw_name = cells[name_col].text.strip()
+                    string = cells[str_col].text.strip()
+                    desc = to_desc(cells[desc_col].text.strip())
+                    name = re.sub(junk, '', raw_name).upper().replace(' ', '_')
+                    name = self.replace(name, string)
+                    if name in found:
+                        raise KeyError("Duplicate name: %s (%s, %s)" % (name, string, found[name]))
+                    found[name] = (string, desc)
+        return found
+
 
 def to_desc(text):
     text = re.sub(r'\[[^]]*]', '', text)
@@ -32,35 +86,17 @@ class Advancements(EnumDesc):
     def __init__(self):
         super().__init__('Advancement', 'https://minecraft.fandom.com/wiki/Advancement#List_of_advancements')
 
-    def generate(self):
-        soup = self.fetch()
-        tables = soup.find_all('table', attrs={'data-description': 'advancements'})
-        found = {}
-        for table in tables:
-            rows = table.find_all('tr')
-            for row in rows:
-                headers = row.find_all('th')
-                if headers:
-                    for i in range(0, len(headers)):
-                        th = headers[i]
-                        text = th.text.strip()
-                        if text == 'Advancement':
-                            name_col = i
-                        elif text.startswith('Resource'):
-                            str_col = i
-                        elif text.find('description') >= 0:
-                            desc_col = i
-                else:
-                    cells = row.find_all('td')
-                    raw_name = cells[name_col].text.strip()
-                    string = cells[str_col].text.strip()
-                    desc = to_desc(cells[desc_col].text.strip())
-                    name = re.sub(junk, '', raw_name).upper().replace(' ', '_')
-                    name = self.replace(name, string)
-                    if name in found:
-                        raise KeyError("Duplicate name: %s (%s, %s)" % (name, string, found[name]))
-                    found[name] = (string, desc)
-        return found
+    def is_name_col(self, text: str):
+        return text == 'Advancement'
+
+    def is_string_col(self, text: str):
+        return text.startswith('Resource')
+
+    def is_desc_col(self, text: str):
+        return text.find('description') >= 0
+
+    def filter_out(self, text: str):
+        pass
 
     def replace(self, name, string):
         if name == 'THE_END' and string.startswith('story'):
@@ -71,6 +107,26 @@ class Advancements(EnumDesc):
 class Effects(EnumDesc):
     def __init__(self):
         super().__init__('Effects', 'https://minecraft.fandom.com/wiki/Effect?so=search#Effect_list')
+
+    def is_name_col(self, text: str):
+        return text == 'Display name'
+
+    def is_string_col(self, text: str):
+        return text.startswith('Name')
+
+    def is_desc_col(self, text: str):
+        return text.find('Effect') >= 0
+
+    def is_filter_col(self, text: str):
+        return text.find('ID (J.E.)')
+
+    def filter_out(self, text: str):
+        return text == 'N/A'
+
+    def replace(self, name, string):
+        if name == 'THE_END' and string.startswith('story'):
+            return 'ENTER_THE_END'
+        return name
 
     def generate(self):
         soup = self.fetch()
