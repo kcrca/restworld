fbc2cf2ef844527c6af73c4b8eec2b92e34b2100
Ken Arnold <arnold@peak.im>
2022-05-29 00:47:23 -0700

    renam 'string' to 'value' in generating enums

--- restworld/build_enums.py
+++ restworld/build_enums.py
@@ -19,7 +19,7 @@ class EnumDesc(ABC):
     def generate(self):
         raise RuntimeError('oops')
 
-    def replace(self, name: str, string: str):
+    def replace(self, name: str, value: str):
         return name
 
     @abstractmethod
@@ -27,7 +27,7 @@ class EnumDesc(ABC):
         pass
 
     @abstractmethod
-    def is_string_col(self, text: str):
+    def is_value_col(self, text: str):
         pass
 
     @abstractmethod
@@ -55,8 +55,8 @@ class EnumDesc(ABC):
                         text = th.text.strip()
                         if self.is_name_col(text):
                             name_col = i
-                        elif self.is_string_col(text):
-                            str_col = i
+                        elif self.is_value_col(text):
+                            value_col = i
                         elif self.is_desc_col(text):
                             desc_col = i
                         elif self.is_filter_col(text):
@@ -66,13 +66,13 @@ class EnumDesc(ABC):
                     if filter_col is not None and self.filter_out(cells[filter_col]):
                         continue
                     raw_name = clean(cells[name_col].text)
-                    string = clean(cells[str_col].text)
+                    value = clean(cells[value_col].text)
                     desc = to_desc(clean(cells[desc_col].text))
                     name = re.sub(junk, '', raw_name).upper().replace(' ', '_')
-                    name = self.replace(name, string)
+                    name = self.replace(name, value)
                     if name in found:
-                        raise KeyError("Duplicate name: %s (%s, %s)" % (name, string, found[name]))
-                    found[name] = (string, desc)
+                        raise KeyError("Duplicate name: %s (%s, %s)" % (name, value, found[name]))
+                    found[name] = (value, desc)
         return found
 
 
@@ -95,7 +95,7 @@ class Advancements(EnumDesc):
     def is_name_col(self, text: str):
         return text == 'Advancement'
 
-    def is_string_col(self, text: str):
+    def is_value_col(self, text: str):
         return text.startswith('Resource')
 
     def is_desc_col(self, text: str):
@@ -104,8 +104,8 @@ class Advancements(EnumDesc):
     def filter_out(self, text: str):
         pass
 
-    def replace(self, name, string):
-        if name == 'THE_END' and string.startswith('story'):
+    def replace(self, name, value):
+        if name == 'THE_END' and value.startswith('story'):
             return 'ENTER_THE_END'
         return name
 
@@ -117,7 +117,7 @@ class Effects(EnumDesc):
     def is_name_col(self, text: str):
         return text == 'Display name'
 
-    def is_string_col(self, text: str):
+    def is_value_col(self, text: str):
         return text.startswith('Name')
 
     def is_desc_col(self, text: str):
@@ -129,71 +129,36 @@ class Effects(EnumDesc):
     def filter_out(self, text: str):
         return text == 'N/A'
 
-    def replace(self, name, string):
-        if name == 'THE_END' and string.startswith('story'):
+    def replace(self, name, value):
+        if name == 'THE_END' and value.startswith('story'):
             return 'ENTER_THE_END'
         return name
 
 
-# class Enchantments(EnumDesc):
-#     def __init__(self):
-#         super().__init__('Enchantments', 'https://minecraft.fandom.com/wiki/Enchanting#Summary_of_enchantments')
-#
-#     def is_name_col(self, text: str):
-#         return text == 'Display name'
-#
-#     def is_string_col(self, text: str):
-#         return text.startswith('Name')
-#
-#     def is_desc_col(self, text: str):
-#         return text.find('Effect') >= 0
-#
-#     def is_filter_col(self, text: str):
-#         return text.find('ID (J.E.)')
-#
-#     def filter_out(self, text: str):
-#         return text == 'N/A'
-#
-#     def replace(self, name, string):
-#         if name == 'THE_END' and string.startswith('story'):
-#             return 'ENTER_THE_END'
-#         return name
-#
-#     def generate(self):
-#         soup = self.fetch()
-#         tables = soup.find_all('table', attrs={'data-description': 'Effects'})
-#         found = {}
-#         for table in tables:
-#             rows = table.find_all('tr')
-#             for row in rows:
-#                 headers = row.find_all('th')
-#                 if headers:
-#                     for i in range(0, len(headers)):
-#                         th = headers[i]
-#                         text = th.text.strip()
-#                         if text == 'Display name':
-#                             name_col = i
-#                         elif text.startswith('Name'):
-#                             str_col = i
-#                         elif text.find('Effect') >= 0:
-#                             desc_col = i
-#                         elif text.find('ID (J.E.)'):
-#                             id_col = i
-#                 else:
-#                     cells = row.find_all('td')
-#                     id = cells[id_col].text.strip()
-#                     if id == 'N/A':
-#                         continue
-#                     raw_name = cells[name_col].text.strip()
-#                     string = cells[str_col].text.strip()
-#                     string = re.sub(r'\[.*', '', string, flags=re.DOTALL)
-#                     desc = to_desc(cells[desc_col].text.strip())
-#                     name = re.sub(junk, '', raw_name).upper().replace(' ', '_')
-#                     name = self.replace(name, string)
-#                     if name in found:
-#                         raise KeyError("Duplicate name: %s (%s, %s)" % (name, string, found[name]))
-#                     found[name] = (string, desc)
-#         return found
+class Enchantments(EnumDesc):
+    def __init__(self):
+        super().__init__('Enchantments', 'https://minecraft.fandom.com/wiki/Enchanting#Summary_of_enchantments',
+                         'Summary of enchantments ')
+
+    def is_name_col(self, text: str):
+        return text == 'Name'
+
+    def is_value_col(self, text: str):
+        return text.startswith('Name')
+
+    def is_desc_col(self, text: str):
+        return text.find('Effect') >= 0
+
+    def is_filter_col(self, text: str):
+        return text.find('ID (J.E.)')
+
+    def filter_out(self, text: str):
+        return text == 'N/A'
+
+    def replace(self, name, value):
+        if name == 'THE_END' and value.startswith('story'):
+            return 'ENTER_THE_END'
+        return name
 
 
 if __name__ == '__main__':
