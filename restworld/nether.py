1419b90d7081a057e225e12ba650fd3bde4035c6
Ken Arnold <arnold@peak.im>
2023-02-26 19:09:40 -0800

    piglins riding hoglins

--- restworld/nether.py
+++ restworld/nether.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pynecraft.base import NORTH, SOUTH, WEST, r
-from pynecraft.commands import Entity, data, e, execute, setblock
+from pynecraft.commands import Entity, data, e, execute, function, ride, s, setblock
 from pynecraft.simpler import Item, WallSign
 from restworld.rooms import Room, label
 from restworld.world import kill_em, main_clock, restworld
@@ -33,15 +33,40 @@ def room():
     room.function('piglin_brute_init').add(
         placer(r(0, 2, 0), lhs_dir, adults=True).summon(
             Entity('Piglin Brute', {'HandItems': [Item.nbt_for('golden_axe')]})))
+
+    hoglin_riders = room.score('hoglin_riders')
+    room.function('piglin_init').add(label(r(-1, 2, -3), 'Riders'))
+
+    def add_rider(riders, num, on_tag):
+        new_tag = f'hoglin_rider_{num}'
+        riders.add(execute().if_().entity(e().tag(*on_tag).limit(1)).run(
+            Entity('piglin', {'NoAI': True, 'NoGravity': True, 'IsBaby': True,
+                              'Tags': [room.name, 'hoglin_rider', new_tag]}).summon(
+                r(0, 2.25 + num, -3), facing=NORTH),
+            ride(e().tag(new_tag).limit(1)).mount(e().tag(*on_tag).limit(1))))
+        return new_tag
+
+    riders = room.function('ride_hoglin', home=False).add(hoglin_riders.set(1))
+    t0 = add_rider(riders, 0, ('hoglin', 'kid'))
+    t1 = add_rider(riders, 1, (t0,))
+    add_rider(riders, 2, (t1,))
+    room.function('unride_hoglin', home=False).add(
+        hoglin_riders.set(0),
+        execute().as_(e().tag('hoglin_rider')).run(ride(s()).dismount()),
+        kill_em(e().tag('hoglin_rider')))
+
     piglins = (Entity('Piglin', nbt={'HandItems': [Item.nbt_for('golden_sword')]}), 'Zombified Piglin')
     hoglins = ('Hoglin', 'Zoglin')
 
     def piglin_loop(step):
         p = placer(r(0, 2, 0), lhs_dir, 3, 3, tags=('piglin',))
         yield p.summon(hoglins[step.i])
+        if step.i == 0:
+            yield execute().if_().score(hoglin_riders).matches(1).run(function(riders)),
         yield p.summon(step.elem)
 
     room.loop('piglin', main_clock).add(kill_em(e().tag('piglin'))).loop(piglin_loop, piglins)
+
     room.function('strider_init').add(
         placer(r(0, 2, 0), lhs_dir, 0, 3).summon('strider'),
         label(r(6, 2, -5), 'Change Height'),
