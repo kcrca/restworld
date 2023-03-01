3bdc430394f263efa48827e06438da980d97748c
Ken Arnold <arnold@peak.im>
2023-02-28 17:13:21 -0800

    update map room

--- restworld/maps.py
+++ restworld/maps.py
@@ -3,11 +3,15 @@ from __future__ import annotations
 import re
 from dataclasses import dataclass
 
-from pynecraft.base import NORTH, SOUTH, WEST, r
+from pynecraft.base import NORTH, Nbt, NbtDef, SOUTH, WEST, r
 from pynecraft.commands import Block, Entity, JsonText, clone, data, e, fill, item, setblock
 from pynecraft.simpler import Book, ItemFrame, Sign, WallSign
 from restworld.rooms import Room, ensure, label
 from restworld.world import main_clock, restworld
+
+
+def name_nbt(name: str) -> NbtDef:
+    return {'tag': {'display': {'Name': JsonText.text(name)}}}
 
 
 def room():
@@ -36,30 +40,29 @@ def room():
         fill(r(0, -1, -3), r(9, -1, 3), 'redstone_block').replace('glass')
     )
     room.function('maps_room_exit').add(fill(r(0, -1, -3), r(9, -1, 3), 'glass').replace('redstone_block'))
-    p_north = room.mob_placer(r(2, 4, -3), SOUTH, -1, adults=True)
+    p_north = room.mob_placer(r(3, 4, -3), SOUTH, -1, adults=True)
     p_mid = room.mob_placer(r(8, 4, -1), WEST, -1, adults=True)
     room.function('maps_room_init', exists_ok=True).add(
-        room.mob_placer(r(3, 5, -3), SOUTH, adults=True).summon(ItemFrame(SOUTH).item(map(31))),
-        p_north.summon(ItemFrame(SOUTH).item(map(29))),
-        p_north.summon(ItemFrame(SOUTH).item(map(26))),
-        p_north.summon(ItemFrame(SOUTH).item(map(30))),
-        room.mob_placer(r(3, 3, -3), SOUTH, adults=True).summon(ItemFrame(SOUTH).item(map(28))),
+        p_north.summon(ItemFrame(SOUTH).item(map(26, name_nbt('Biomes (top)')))),
+        room.mob_placer(r(3, 3, -3), SOUTH, adults=True).summon(
+            ItemFrame(SOUTH).item(map(28, name_nbt('Biomes (bot)')))),
         WallSign((None, 'Biome', 'Sampler'), SOUTH).place(r(4, 3, -3), SOUTH),
 
-        room.mob_placer(r(6, 4, -3), SOUTH, adults=True).summon(ItemFrame(SOUTH).item(map(23))),
+        room.mob_placer(r(6, 4, -3), SOUTH, adults=True).summon(
+            ItemFrame(SOUTH).item(map(23, name_nbt('Battle Arena')))),
         WallSign((None, 'Battle', 'Arena'), SOUTH).place(r(7, 3, -3), SOUTH),
 
-        room.mob_placer(r(8, 5, 0), WEST, adults=True).summon(ItemFrame(WEST).item(map(18))),
-        p_mid.summon(ItemFrame(WEST).item(map(19)).tag('map_icon_frame')),
-        p_mid.summon(ItemFrame(WEST).item(map(14))),
-        p_mid.summon(ItemFrame(WEST).item(map(21))),
-        room.mob_placer(r(8, 3, 0), WEST, adults=True).summon(ItemFrame(WEST).item(map(20))),
+        room.mob_placer(r(8, 5, 0), WEST, adults=True).summon(ItemFrame(WEST).item(map(18, name_nbt('Main (top')))),
+        p_mid.summon(ItemFrame(WEST).item(map(19, name_nbt('Main (left)'))).tag('map_icon_frame')),
+        p_mid.summon(ItemFrame(WEST).item(map(14, name_nbt('Main (center)')))),
+        p_mid.summon(ItemFrame(WEST).item(map(21, name_nbt('Main (right')))),
+        room.mob_placer(r(8, 3, 0), WEST, adults=True).summon(ItemFrame(WEST).item(map(20, name_nbt('Main (bot)')))),
         WallSign((None, 'Center', 'Area')).place(r(8, 3, 1), WEST),
 
-        room.mob_placer(r(6, 4, 3), NORTH, adults=True).summon(ItemFrame(NORTH).item(map(32))),
-        WallSign((None, 'Connected', 'Textures'), NORTH).place(r(5, 3, 3), NORTH),
+        room.mob_placer(r(6, 4, 3), NORTH, adults=True).summon(ItemFrame(NORTH).item(map(32, name_nbt('Optifine')))),
+        WallSign(('Optifine', 'Connected', 'Textures,', 'Mob Textures'), NORTH).place(r(5, 3, 3), NORTH),
 
-        room.mob_placer(r(3, 4, 3), NORTH, adults=True).summon(ItemFrame(NORTH).item(map(22))),
+        room.mob_placer(r(3, 4, 3), NORTH, adults=True).summon(ItemFrame(NORTH).item(map(22, name_nbt('Photo')))),
         WallSign((None, 'Photo', 'Area'), NORTH).place(r(2, 3, 3), NORTH),
 
         setblock(r(8, 2, 2), 'cartography_table'),
@@ -85,7 +88,24 @@ def room():
 
     room.loop('map_icons', main_clock).loop(icon_loop, map_icons.items()).add(
         item().replace().entity(icon_frame, 'container.0').from_().block(chest_pos, map_slot))
-    room.function('map_icons_init').add(WallSign(()).place(r(0, 4, -1), WEST))
+    map_chest_pos = r(0, -5, 1)
+    room.function('map_icons_init').add(
+        WallSign(()).place(r(0, 4, -1), WEST),
+        #        #/data modify block 68 94 0 Items[0] merge value {tag:{display: {Name:"\"Test\""}}}
+
+        data().modify(map_chest_pos, 'Items[{Slot:6b}]').merge().value(name_nbt('Optifine')),
+        data().modify(map_chest_pos, 'Items[{Slot:20b}]').merge().value(name_nbt('Photo')),
+
+        data().modify(map_chest_pos, 'Items[{Slot:4b}]').merge().value(name_nbt('Main (top)')),
+        data().modify(map_chest_pos, 'Items[{Slot:12b}]').merge().value(name_nbt('Main (left)')),
+        data().modify(map_chest_pos, 'Items[{Slot:13b}]').merge().value(name_nbt('Main (center)')),
+        data().modify(map_chest_pos, 'Items[{Slot:14b}]').merge().value(name_nbt('Main (right)')),
+        data().modify(map_chest_pos, 'Items[{Slot:22b}]').merge().value(name_nbt('Main (bot)')),
+
+        data().modify(map_chest_pos, 'Items[{Slot:1b}]').merge().value(name_nbt('Biomes (top)')),
+        data().modify(map_chest_pos, 'Items[{Slot:2b}]').merge().value(name_nbt('Biomes (bot)')),
+        data().modify(map_chest_pos, 'Items[{Slot:24b}]').merge().value(name_nbt('Battle Arena)')),
+    )
 
     room.function('apologia', home=False).add(
         ensure(r(0, 2, 0), Block('lectern', {'facing': WEST, 'has_book': True}),
@@ -112,5 +132,8 @@ def apologia():
     return book
 
 
-def map(num: int):
-    return 'filled_map', {}, {'tag': {'map': num}}
+def map(num: int, added_nbt: NbtDef = None):
+    nbt = Nbt({'tag': {'map': num}})
+    if added_nbt:
+        nbt = nbt.merge(added_nbt)
+    return 'filled_map', {}, nbt
