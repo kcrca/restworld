b32ac609edc3d5b081e7d74887618c65f373c763
Ken Arnold <arnold@peak.im>
2023-12-03 14:29:01 -0800

    tweak center mobs

--- restworld/center.py
+++ restworld/center.py
@@ -74,16 +74,16 @@ def room():
         kill(e().tag('mob_display')),
 
         summon(Entity('cow', silent).tag('mob_display'), r(-6, 2.5, 0), all),
-        summon(Entity('polar_bear', silent).tag('mob_display'), r(-6, 2.5, 0), all),
         summon(Entity('panda', {'MainGene': 'playful', 'Silent': True}).tag('mob_display'), r(-6, 2.5, 0), all),
         summon(Horse('horse', Horse.Color.CHESTNUT, Horse.Markings.WHITE, silent).tag('mob_display'), r(-6, 2.5, 0),
                all),
         summon(Entity('camel', silent).tag('mob_display'), r(-6, 2.5, 0), all),
 
-        summon(Entity('sniffer', silent).tag('mob_display', 'keeper'), r(6, 2.5, 0), all),
+        summon(Entity('polar_bear', silent).tag('mob_display', 'keeper'), r(6, 2.5, 0), all),
         summon(Entity('llama', silent).tag('mob_display'), r(6, 2.5, 0), all),
         summon(Entity('mooshroom', silent).tag('mob_display'), r(6, 2.5, 0), all),
         summon(Entity('pig', silent).tag('mob_display'), r(6, 2.5, 0), all),
+        summon(Entity('breeze', silent).tag('mob_display'), r(6, 2.5, 0), all),
 
         trim_stand.summon(r(10.51, 2, 0), facing=NORTH),
 
