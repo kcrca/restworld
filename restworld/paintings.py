106a562b445f603e0dd639b8c53a40b3d6b754db
Ken Arnold <arnold@peak.im>
2024-05-13 14:35:27 -0700

    step 1 on fixin gup pantings: the wall() method

--- restworld/paintings.py
+++ restworld/paintings.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from pynecraft.base import NORTH, r
-from pynecraft.commands import e, fill, kill, summon
-from pynecraft.simpler import Painting, WallSign
+from pynecraft.base import NORTH, ROTATION_270, SOUTH, WEST, as_facing, r
+from pynecraft.commands import JsonText, e, fill, kill, summon
+from pynecraft.simpler import TextDisplay
+from pynecraft.values import PAINTING_GROUP, paintings
 from restworld.rooms import Room
 from restworld.world import restworld
 
@@ -11,54 +12,75 @@ from restworld.world import restworld
 def room():
     room = Room('paintings', restworld, NORTH, (None, 'Paintings'))
     room.reset_at((0, 3))
+    unused = set(PAINTING_GROUP)
 
-    def painting(id, facing, x, z, sx=0, sy=0, sz=0, note=''):
-        thing = Painting(id)
-        px, pz = ((-1, 0), (0, -1), (1, 0), (0, 1))[facing]
-        px += sx
-        pz += sz
-        dir = ('south', 'west', 'north', 'east')[facing]
-        yield summon('painting', r(x, 3, z), {'variant': thing.variant, 'facing': facing, 'Tags': ['painting']})
-        yield WallSign((None, thing.info.name, note)).place(r(x + px, 2 + sy, z + pz), dir)
+    def wall(ids, facing, x, z):
+        if isinstance(ids, str):
+            ids = (ids,)
+        facing = as_facing(facing)
+        moving = facing.turn(ROTATION_270)
+        for id in ids:
+            if isinstance(id, int):
+                x += id * moving.dx
+                z += id * moving.dz
+                continue
+            thing = paintings[id]
+            yield from painting(id, facing, moving, x, z)
+            x += (thing.size[0] + 1) * moving.dx
+            z += (thing.size[0] + 1) * moving.dz
+
+    def painting(id, facing, moving, x, z, sx=0, sy=0, sz=0, note=''):
+        thing = paintings[id]
+        px, pz = (0, 0)
+        y = 2 if thing.size[1] >= 4 else 3
+        if thing.size[0] > 2 and thing.size[1] > 2:
+            px += moving.dx
+            pz += moving.dz
+            y += px
+        yield summon('painting', r(x + px, y, z + pz),
+                     {'variant': thing.name, 'facing': facing.painting_number, 'Tags': ['painting']})
+        yield summon(TextDisplay(nbt={'Rotation': facing.rotation}).scale(0.5).text(JsonText().text(thing.value)))
+        # yield WallSign((None, thing.value, note)).place(r(x - moving.dx, 2, z - moving.dz), facing)
+        unused.remove(thing.value)
 
     room.function('all_paintings_init').add(
         kill(e().tag('painting')),
-        fill(r(-2, 2, 0), r(16, 6, 22), 'air').replace('oak_wall_sign'),
+        kill(e().type('item')),
+        fill(r(-2, 2, 0), r(16, 6, 26), 'air').replace('oak_wall_sign'),
 
-        painting('Burning Skull', 0, 0, 0, sx=-1),
-        painting('Pointer', 0, 11, 0, sx=4, sy=1),
+        wall(('Skull On Fire', 7, 'Pointer'), SOUTH, -1, 0),
+        wall(('Skull and Roses', 'The void', 'The Stage Is Set', 'Wither', 'Bust', 'Match'), WEST, 14, 2),
 
-        painting('Skull and Roses', 1, 14, 2),
-        painting('Bust', 1, 14, 5),
-        painting('Void', 1, 14, 8),
-        painting('Match', 1, 14, 11),
-        painting('Stage', 1, 14, 14),
-        painting('Wither', 1, 14, 17),
-
-        painting('Pigscene', 2, 12, 20, sx=1),
-        painting('Earth', 2, 8, 20, note='(unused)'),
-        painting('Wind', 2, 5, 20, note='(unused)'),
-        painting('Graham', 2, 1, 20),
-        painting('Wanderer', 2, -1, 20),
-
-        painting('Fighters', 3, -2, 16, sz=1),
-        painting('Donkey Kong', 3, -2, 10, sz=1),
-        painting('Skeleton', 3, -2, 4, sz=1),
-
-        painting('Wasteland', 2, 4, 4),
-        painting('Kebab', 2, 6, 4),
-        painting('Alban', 2, 8, 4),
-        painting('Bomb', 0, 9, 6),
-        painting('Plant', 0, 7, 6),
-        painting('Sunset', 0, 4, 6),
-
-        painting('Creebet', 2, 5, 9),
-        painting('Courbet', 2, 8, 9),
-        painting('Pool', 0, 7, 11),
-        painting('Sea', 0, 4, 11),
-
-        painting('Aztec2', 2, 4, 14),
-        painting('Aztec', 2, 7, 14),
-        painting('Water', 0, 4, 16, note='(unused)'),
-        painting('Fire', 0, 7, 16, note='(unused)')
+        # painting('Pigscene', 2, 12, 26, sx=1),
+        # painting('Earth', 2, 8, 26, note='(unused)'),
+        # painting('Wind', 2, 5, 26, note='(unused)'),
+        # painting('Graham', 2, 2, 26),
+        # painting('Wanderer', 2, 0, 26),
+        # painting('Prairie Ride', 2, -2, 26),
+        #
+        # painting('Unpacked', 3, -2, 18, sz=1),
+        # painting('Fighters', 3, -2, 13, sz=1),
+        # painting('Kong', 3, -2, 8, sz=1),
+        # painting('Mortal Coil', 3, -2, 3, sz=1),
+        #
+        # painting('Wasteland', 2, 4, 4),
+        # painting('Kebab med tre pepperoni', 2, 6, 4),
+        # painting('Albanian', 2, 8, 4),
+        # painting('Target Successfully Bombed', 0, 9, 6),
+        # painting('Paradisträd', 0, 7, 6),
+        # painting('sunset_dense', 0, 4, 6),
+        #
+        # painting('Creebet', 2, 5, 9),
+        # painting('Bonjour Monsieur Courbet', 2, 8, 9),
+        # painting('The Pool', 0, 7, 11),
+        # painting('Seaside', 0, 4, 11),
+        #
+        # painting('Meditative', 2, 4, 20),
+        # painting('de_aztec 2', 2, 6, 20),
+        # painting('de_aztec', 2, 8, 20),
+        # painting('Water', 0, 4, 22, note='(unused)'),
+        # painting('Fire', 0, 7, 22, note='(unused)')
     )
+
+    if unused:
+        print('WARNING: Unused paintings: ' + ', '.join(unused))
