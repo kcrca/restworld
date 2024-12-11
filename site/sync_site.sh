3aba2a28f1095311f7abf4194a7ac832e5cfdae5
Ken Arnold <arnold@peak.im>
2024-12-11 10:32:21 -0800

    remove reundant exclusion

--- site/sync_site.sh
+++ site/sync_site.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 ./build.sh
 ./favicon.sh
 dst_dir="restworld"
-rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' --exclude='favicon.p*' . kcrca_claritypack@ssh.nyc1.nearlyfreespeech.net:$dst_dir/
+rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='favicon.p*' . kcrca_claritypack@ssh.nyc1.nearlyfreespeech.net:$dst_dir/
